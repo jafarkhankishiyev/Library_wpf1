fc5390570e694b77b13f29cb499af705829af537
jafarkhankishiyev <djafarxankishiev@gmail.com>
2024-01-12 17:02:35 +0400

    Transitioned completely to MVVM

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -11,7 +11,6 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Npgsql;
 using Library_wpf.ViewModelNameSpace;
-using Library_wpf.UI;
 
 namespace Library_wpf;
 
@@ -20,132 +19,9 @@ namespace Library_wpf;
 /// </summary>
 public partial class MainWindow : Window
 {
-    //    private SortUI _sort;
-    /*
-    public bool isNameSortClicked = false;
-    public bool isAuthorSortClicked = false;
-    public bool isGenreSortClicked = false;
-    public bool isYearSortClicked = false;
-    private List<Book> booksToSort = new List<Book>();
-    */
     public MainWindow()
     {
-        //_sort = new SortUI(this);
         InitializeComponent();
         DataContext = new LibraryViewModel();
     }
-    private void NameColumnHeader_Click(object sender, RoutedEventArgs e)
-    {
-    }
-    private void AuthorColumnHeader_Click(object sender, RoutedEventArgs e)
-    {
-    }
-    private void GenreColumnHeader_Click(object sender, RoutedEventArgs e)
-    {
-    }
-    private void YearColumnHeader_Click(object sender, RoutedEventArgs e)
-    {
-    }
-    private void BookList_SelectionChanged(object sender, SelectionChangedEventArgs e)
-    {
-        if (bookList.SelectedItems.Count == 1)
-        {
-            addBookButton.IsEnabled = false;
-            deleteBookButton.IsEnabled = true;
-            editBookButton.IsEnabled = true;
-        }
-        else if (bookList.SelectedItems.Count > 1)
-        {
-            addBookButton.IsEnabled = false;
-            deleteBookButton.IsEnabled = true;
-            editBookButton.IsEnabled = false;
-        }
-        else
-        {
-            addBookButton.IsEnabled = true;
-            deleteBookButton.IsEnabled = false;
-            editBookButton.IsEnabled = false;
-        }
-    }
-
-
-    /*
-    public void nameColumnHeader_Click(object sender, RoutedEventArgs e)
-    {
-        if (isNameSortClicked)
-        {
-            isNameSortClicked = false;
-            //_ = _libraryviewmodel.ShowBooks();
-        }
-        else
-        {
-            isAuthorSortClicked = false;
-            isGenreSortClicked = false;
-            isYearSortClicked = false;
-            isNameSortClicked = true;
-            booksToSort = bookList.ItemsSource as List<Book>;
-            booksToSort.Sort((x, y) => string.Compare(x.Name, y.Name));
-            bookList.ItemsSource = new List<Book>();
-            bookList.ItemsSource = booksToSort;
-        }
-    }
-    public void authorColumnHeader_Click(object sender, RoutedEventArgs e)
-    {
-        if (isAuthorSortClicked)
-        {
-            isAuthorSortClicked = false;
-            //  _ = _libraryviewmodel.ShowBooks();
-        }
-        else
-        {
-            isNameSortClicked = false;
-            isGenreSortClicked = false;
-            isYearSortClicked = false;
-            isAuthorSortClicked = true;
-            booksToSort = bookList.ItemsSource as List<Book>;
-            booksToSort.Sort((x, y) => string.Compare(x.Author, y.Author));
-            bookList.ItemsSource = new List<Book>();
-            bookList.ItemsSource = booksToSort;
-        }
-    }
-    public void genreColumnHeader_Click(object sender, RoutedEventArgs e)
-    {
-        if (isGenreSortClicked)
-        {
-            isGenreSortClicked = false;
-            //  _ = _libraryviewmodel.ShowBooks();
-        }
-        else
-        {
-            isNameSortClicked = false;
-            isAuthorSortClicked = false;
-            isYearSortClicked = false;
-            isGenreSortClicked = true;
-            booksToSort = bookList.ItemsSource as List<Book>;
-            booksToSort.Sort((x, y) => string.Compare(x.Genre, y.Genre));
-            bookList.ItemsSource = new List<Book>();
-            bookList.ItemsSource = booksToSort;
-        }
-    }
-    public void yearColumnHeader_Click(object sender, RoutedEventArgs e)
-    {
-        if (isYearSortClicked)
-        {
-            isYearSortClicked = false;
-            //  _ = _libraryviewmodel.ShowBooks();
-        }
-        else
-        {
-            isNameSortClicked = false;
-            isAuthorSortClicked = false;
-            isGenreSortClicked = false;
-            isYearSortClicked = true;
-            booksToSort = bookList.ItemsSource as List<Book>;
-            //booksToSort.Sort((x, y) => string.Compare(x.Release, y.Release));
-            booksToSort = booksToSort.OrderByDescending(x => x.Release).ToList();
-            bookList.ItemsSource = new List<Book>();
-            bookList.ItemsSource = booksToSort;
-        }
-    }
-    */
 }
--- ViewModel/LibraryViewModel.cs
+++ ViewModel/LibraryViewModel.cs
@@ -7,7 +7,6 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Controls;
 using System.Windows;
-using Library_wpf.UI;
 using Library_wpf;
 using System.Runtime.CompilerServices;
 
@@ -15,12 +14,15 @@ namespace Library_wpf.ViewModelNameSpace
 {
     class LibraryViewModel : INotifyPropertyChanged
     {
+        //fields
         public event PropertyChangedEventHandler? PropertyChanged;
         private BookDB _bookDB;
         private RelayCommand addBookCommand;
         private RelayCommand deleteBookCommand;
         private RelayCommand editBookCommand;
         private RelayCommand saveCommand;
+        private RelayCommand sortByNameCommand;
+        private RelayCommand selectionChangedCommand;
         private string nameText;
         private string authorText;
         private string genreText;
@@ -39,10 +41,15 @@ namespace Library_wpf.ViewModelNameSpace
         private string yearWarningText;
         private bool nameTextBoxFocus;
         private object selectedBook;
+        private List<object> selectedBooks;
         private bool dynamicVisGridEnabled;
         private List<Book> bookListSource;
         private bool BookList_SelectionChanged;
+        private RelayCommand sortByAuthorCommand;
+        private RelayCommand sortByGenreCommand;
+        private RelayCommand sortByYearCommand;
 
+        //properties
         public string NameText { get { return nameText; } 
             set
             {
@@ -146,6 +153,19 @@ namespace Library_wpf.ViewModelNameSpace
             set 
             {
                 selectedBook = value;
+                if(selectedBook == null)
+                {
+                    ClearText();
+                    AddButtonEnabled = true;
+                    EditButtonEnabled = false;
+                    DeleteButtonEnabled = false;
+                } 
+                else
+                {
+                    AddButtonEnabled = false;
+                    EditButtonEnabled = true;
+                    DeleteButtonEnabled = true;
+                }
                 OnPropertyChanged("SelectedBook");
             } 
         }
@@ -155,124 +175,31 @@ namespace Library_wpf.ViewModelNameSpace
                 OnPropertyChanged("BookListSource");
             }
         }
-        public RelayCommand AddBookCommand 
-        { 
-            get { return addBookCommand ?? (addBookCommand = new RelayCommand(obj =>
-                    {
-                        NameText = string.Empty;
-                        AuthorText = string.Empty;
-                        GenreText = string.Empty;
-                        YearText = string.Empty;
-                        DynamicVisGridEnabled = true;
-                        NameTextBoxFocus = true;
-                        isAddBookButtonClicked = true;
-                        isEditBookButtonClicked = false;
-                        SaveButtonEnabled = true;
-                    }));
-                }
-        }
-        public RelayCommand DeleteBookCommand
-        { get { return deleteBookCommand ?? (deleteBookCommand = new RelayCommand(async obj => 
-                    {
-                        if (SelectedBook != null)
-                        {
-                            DynamicVisGridEnabled = false;
-                            ClearText();
-                            Book selectedItem = SelectedBook as Book;
-                            int deleteResult = await _bookDB.DeleteBook(selectedItem);
-                            DeleteButtonEnabled = false;
-                            EditButtonEnabled = false;
-                            _ = ShowBooks();
-                            MessageBox.Show($"Deleted {deleteResult} object(s).");
-                        }
-                    })); 
-            } 
-        }
-        public RelayCommand EditBookCommand
-        {
-            get {
-                return editBookCommand ?? (editBookCommand = new RelayCommand(obj =>
-                    {
-                        isEditBookButtonClicked = true;
-                        isAddBookButtonClicked = false;
-                        DynamicVisGridEnabled = true;
-                        Book oldBook = SelectedBook as Book;
-                        NameText = oldBook.Name;
-                        AuthorText = oldBook.Author;
-                        GenreText = oldBook.Genre;
-                        YearText = oldBook.Release.ToString();
-                        SaveButtonEnabled = true;
-                    }));
-                }   
-        }
-        public RelayCommand SaveCommand
-        {
-            get
-            {
-                return saveCommand ?? (saveCommand = new RelayCommand(async obj =>
-                {
-                    isButton1Clicked = true;
-                    Book book = new Book();
-                    book.Name = NameText;
-                    book.Author = AuthorText;
-                    book.Genre = GenreText;
-                    int result;
-                    bool releaseCheck = Int32.TryParse(YearText, out result);
-                    if (releaseCheck)
-                    {
-                        book.Release = result;
-                    }
-                    else
-                    {
-                        book.Release = 0;
-                    }
-                    int validateNum = Validate(book);
-                    if (validateNum == 1)
-                    {
-                        if (isAddBookButtonClicked)
-                        {
-                            int number = await _bookDB.AddBook(book);
-                            MessageBox.Show($"Added {number} object.");
-                            isButton1Clicked = false;
-                            DynamicVisGridEnabled = false;
-                            ClearText();
-                            isAddBookButtonClicked = false;
-                        }
-                        else if (isEditBookButtonClicked)
-                        {
-                            string dataChoicePrepared = "";
-                            Book oldBook = SelectedBook as Book;
-                            int number = await _bookDB.EditBook(oldBook, book);
-                            MessageBox.Show($"Modified {number} object.");
-                            isEditBookButtonClicked = false;
-                            DynamicVisGridEnabled = false;
-                            ClearText();
-                        }
-                        isButton1Clicked = false;
-                        EditButtonEnabled = false;
-                        DeleteButtonEnabled = false;
-                        _ = ShowBooks();
-                        SaveButtonEnabled = false;
-                    }
-                    else
-                    {
-                        isButton1Clicked = false;
-                    }
-                }));
-            }
-        }
+        public RelayCommand AddBookCommand { get { return addBookCommand ?? (addBookCommand = new RelayCommand(obj => AddBookCommandMethod())); }}
+        public RelayCommand DeleteBookCommand { get { return deleteBookCommand ?? (deleteBookCommand = new RelayCommand(obj => DeleteBookCommandMethod())); }}
+        public RelayCommand EditBookCommand { get { return editBookCommand ?? (editBookCommand = new RelayCommand(obj => EditBookCommandMethod())); }}
+        public RelayCommand SaveCommand{ get { return saveCommand ?? (saveCommand = new RelayCommand(obj => SaveCommandMethod())); }}
+        public RelayCommand SortByNameCommand { get { return sortByNameCommand ?? (sortByNameCommand = new RelayCommand(obj => SortByNameCommandMethod())); }}
+        public RelayCommand SortByAuthorCommand { get { return sortByAuthorCommand ?? (sortByAuthorCommand = new RelayCommand(obj => SortByAuthorCommandMethod())); }}
+        public RelayCommand SortByGenreCommand { get { return sortByGenreCommand ?? (sortByGenreCommand = new RelayCommand(obj => SortByGenreCommandMethod())); }}
+        public RelayCommand SortByYearCommand { get { return sortByYearCommand ?? (sortByYearCommand = new RelayCommand(obj => SortByYearCommandMethod())); }}
+        public bool isNameSortClicked = false;
+        public bool isAuthorSortClicked = false;
+        public bool isGenreSortClicked = false;
+        public bool isYearSortClicked = false;
+        private List<Book> booksToSort = new List<Book>();
+
         public LibraryViewModel()
         {
             _bookDB = new BookDB();
-            _ = ShowBooks();
+            _ = GetBooks();
             AddButtonEnabled = true;
             EditButtonEnabled = false; 
             DeleteButtonEnabled = false;
         }
-        public async Task ShowBooks()
+        public async Task GetBooks()
         {
-            List<Book> books = await _bookDB.GetBooksAsync();
-            BookListSource = books;
+            BookListSource = await _bookDB.GetBooksAsync();
         }
         public void ClearText()
         {
@@ -314,6 +241,175 @@ namespace Library_wpf.ViewModelNameSpace
             else
             {
                 return 1;
+            }
+        }
+
+        //button commands
+        public async void SaveCommandMethod()
+        {
+            isButton1Clicked = true;
+            Book book = new Book();
+            book.Name = NameText;
+            book.Author = AuthorText;
+            book.Genre = GenreText;
+            int result;
+            bool releaseCheck = Int32.TryParse(YearText, out result);
+            if (releaseCheck)
+            {
+                book.Release = result;
+            }
+            else
+            {
+                book.Release = 0;
+            }
+            int validateNum = Validate(book);
+            if (validateNum == 1)
+            {
+                if (isAddBookButtonClicked)
+                {
+                    int number = await _bookDB.AddBook(book);
+                    MessageBox.Show($"Added {number} object.");
+                    isButton1Clicked = false;
+                    DynamicVisGridEnabled = false;
+                    ClearText();
+                    isAddBookButtonClicked = false;
+                }
+                else if (isEditBookButtonClicked)
+                {
+                    string dataChoicePrepared = "";
+                    Book oldBook = SelectedBook as Book;
+                    int number = await _bookDB.EditBook(oldBook, book);
+                    MessageBox.Show($"Modified {number} object.");
+                    isEditBookButtonClicked = false;
+                    DynamicVisGridEnabled = false;
+                    ClearText();
+                }
+                isButton1Clicked = false;
+                EditButtonEnabled = false;
+                DeleteButtonEnabled = false;
+                _ = GetBooks();
+                SaveButtonEnabled = false;
+            }
+            else
+            {
+                isButton1Clicked = false;
+            }
+        }
+        public void EditBookCommandMethod()
+        {
+            isEditBookButtonClicked = true;
+            isAddBookButtonClicked = false;
+            DynamicVisGridEnabled = true;
+            Book oldBook = SelectedBook as Book;
+            NameText = oldBook.Name;
+            AuthorText = oldBook.Author;
+            GenreText = oldBook.Genre;
+            YearText = oldBook.Release.ToString();
+            SaveButtonEnabled = true;
+        }
+        public async void DeleteBookCommandMethod()
+        {
+            if (SelectedBook != null)
+            {
+                int deleteResult = 0;
+                DynamicVisGridEnabled = false;
+                ClearText();
+                Book selectedItem = SelectedBook as Book;
+                deleteResult = await _bookDB.DeleteBook(selectedItem);
+                DeleteButtonEnabled = false;
+                EditButtonEnabled = false;
+                _ = GetBooks();
+                MessageBox.Show($"Deleted {deleteResult} object(s).");
+            }
+        }
+        public void AddBookCommandMethod()
+        {
+            NameText = string.Empty;
+            AuthorText = string.Empty;
+            GenreText = string.Empty;
+            YearText = string.Empty;
+            DynamicVisGridEnabled = true;
+            NameTextBoxFocus = true;
+            isAddBookButtonClicked = true;
+            isEditBookButtonClicked = false;
+            SaveButtonEnabled = true;
+        }
+        
+        //sort commands
+        public void SortByNameCommandMethod()
+        {
+            if (isNameSortClicked)
+            {
+                isNameSortClicked = false;
+                _ = GetBooks();
+            }
+            else
+            {
+                isAuthorSortClicked = false;
+                isGenreSortClicked = false;
+                isYearSortClicked = false;
+                isNameSortClicked = true;
+                booksToSort = BookListSource as List<Book>;
+                booksToSort.Sort((x, y) => string.Compare(x.Name, y.Name));
+                BookListSource = new List<Book>();
+                BookListSource = booksToSort;
+            }
+        }
+        public void SortByAuthorCommandMethod()
+        {
+            if (isAuthorSortClicked)
+            {
+                isAuthorSortClicked = false;
+                _ = GetBooks();
+            }
+            else
+            {
+                isNameSortClicked = false;
+                isGenreSortClicked = false;
+                isYearSortClicked = false;
+                isAuthorSortClicked = true;
+                booksToSort = BookListSource;
+                booksToSort.Sort((x, y) => string.Compare(x.Author, y.Author));
+                BookListSource = new List<Book>();
+                BookListSource = booksToSort;
+            }
+        }
+        public void SortByGenreCommandMethod()
+        {
+            if (isGenreSortClicked)
+            {
+                isGenreSortClicked = false;
+                _ = GetBooks();
+            }
+            else
+            {
+                isNameSortClicked = false;
+                isAuthorSortClicked = false;
+                isYearSortClicked = false;
+                isGenreSortClicked = true;
+                booksToSort = BookListSource;
+                booksToSort.Sort((x, y) => string.Compare(x.Genre, y.Genre));
+                BookListSource = new List<Book>();
+                BookListSource = booksToSort;
+            }
+        }
+        public void SortByYearCommandMethod()
+        {
+            if (isYearSortClicked)
+            {
+                isYearSortClicked = false;
+                _ = GetBooks();
+            }
+            else
+            {
+                isNameSortClicked = false;
+                isAuthorSortClicked = false;
+                isGenreSortClicked = false;
+                isYearSortClicked = true;
+                booksToSort = BookListSource;
+                booksToSort = booksToSort.OrderByDescending(x => x.Release).ToList();
+                BookListSource = new List<Book>();
+                BookListSource = booksToSort;
             }
         }
         public void OnPropertyChanged([CallerMemberName] string prop = "")
